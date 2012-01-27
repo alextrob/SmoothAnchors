7d8dc1ac60cdff73f98fe51776fa8384f5533db5
Alex <alextrob@gmail.com>
2012-01-27 21:39:26 +1000

    Initial commit

---
+++ SmoothAnchors.js
@@ -0,0 +1,88 @@
+// Copyright (C) 2012 Alex Robinson (http://alextrob.net/)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+// SOFTWARE.
+
+(function( $ ) {
+	$.fn.SmoothAnchors = function() {
+
+		function scrollBodyTo(destination, hash) {
+
+			// Change the hash first, then do the scrolling. This retains the standard functionality of the back/forward buttons.
+
+			var scrollmem = $('body').scrollTop();
+			window.location.hash = hash;
+			$('html,body').scrollTop(scrollmem);
+
+			$("html:not(:animated),body:not(:animated)").animate({scrollTop:destination}, 200);
+			
+		}
+
+		if (typeof $().on == "function") {
+			$(document).on('click', 'a[href^="#"]', function() {
+				
+				var href = $(this).attr("href");
+				
+				if ($(href).length == 0) {
+					
+					var nameSelector = "[name=" + href.replace("#", "") + "]";
+
+					if (href == "#") {
+						scrollBodyTo(0, href);
+					}
+					else if ($(nameSelector).length != 0)  {
+						scrollBodyTo($(nameSelector).offset().top, href);
+					}
+					else {
+						// fine, we'll just follow the original link. gosh.
+						window.location = href;
+					}
+				}
+				else {
+					scrollBodyTo($(href).offset().top, href);
+				}
+				return false;
+			});
+		}
+		else {
+			$('a[href^="#"]').click(function() {
+				var href = $(this).attr("href");
+				
+				if ($(href).length == 0) {
+					
+					var nameSelector = "[name=" + href.replace("#", "") + "]";
+
+					if (href == "#") {
+						scrollBodyTo(0, href);
+					}
+					else if ($(nameSelector).length != 0)  {
+						scrollBodyTo($(nameSelector).offset().top, href);
+					}
+					else {
+						// fine, we'll just follow the original link. gosh.
+						window.location = href;
+					}
+				}
+				else {
+					scrollBodyTo($(href).offset().top, href);
+				}
+				return false;
+			});
+		}
+	};
+})( jQuery );
---
+++ readme.md
@@ -0,0 +1,9 @@
+# SmoothAnchors
+
+## Animated scrolling for internal anchor links
+
+### How to use it
+
+<p>Just include the jQuery library and SmoothAnchors.js, then add <code>$().SmoothAnchors();</code> to your <code>$(document).ready()</code> function.
+
+<pre>&lt;script&nbsp;type=&quot;text/javascript&quot;&nbsp;src=&quot;jquery.min.js&quot;&gt;&lt;/script&gt;<br/>&lt;script&nbsp;type=&quot;text/javascript&quot;&nbsp;src=&quot;SmoothAnchors.js&quot;&gt;&lt;/script&gt;<br/>&lt;script&nbsp;type=&quot;text/javascript&quot;&gt;<br/>&nbsp;&nbsp;&nbsp;&nbsp;$(document).ready(function()&nbsp;{<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$().SmoothAnchors();<br/>&nbsp;&nbsp;&nbsp;&nbsp;});<br/>&lt;/script&gt;</pre>
